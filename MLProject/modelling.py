df54a9050980cf0ad3c99a0e8c50cd99135bd995
Arfwjn <ariefsidik2016@gmail.com>
2025-12-11 18:40:01 +0700

    memperbaiki modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.linear_model import LogisticRegression
 import mlflow
 import mlflow.sklearn
+from sklearn.metrics import accuracy_score, precision_recall_fscore_support
 import joblib
 
 # PATH & KONSTANTA
@@ -29,7 +30,7 @@ mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("CI_Workflow_Klasifikasi_LogisticRegression")
 
 # Autologging
-mlflow.sklearn.autolog() 
+mlflow.sklearn.autolog(disable=True)
 
 # 3. TRAINING MODE
 
@@ -41,13 +42,38 @@ model = LogisticRegression(
     max_iter=500,
 )
 
-model.fit(X_train, y_train)
+current_run_id = "N/A - Run Failed"
 
-joblib.dump(model, PREPROCESSING_DIR + 'lr_model.joblib')
+with mlflow.start_run(run_name="CI_Workflow_LogisticRegression_Manual_Fix") as run:
+    print("Melatih Model Logistic Regression (Manual Log)...")
 
-try:
-    current_run_id = mlflow.active_run().info.run_id
-except:
-    current_run_id = "UNKNOWN - Autologging might have failed"
+    # Training model
+    model.fit(X_train, y_train) 
+    # Prediksi
+    y_pred = model.predict(X_test)
+    
+    # NOTE: menggunakan logging manual karena auto logging selalu gagal
+    mlflow.log_param("max_iter", 500)
+    mlflow.log_param("random_state", 42)
+    
+    # Akurasi
+    accuracy = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("test_accuracy", accuracy)
 
-print(f"Model Training Selesai. Run ID: {mlflow.active_run().info.run_id}")
+    # Presisi, Recall, F1-Score
+    precision, recall, f1_score, _ = precision_recall_fscore_support(
+        y_test, y_pred, average='weighted', zero_division=0
+    )
+    mlflow.log_metric("test_precision_weighted", precision)
+    mlflow.log_metric("test_recall_weighted", recall)
+    mlflow.log_metric("test_f1_score_weighted", f1_score)
+    
+    # Log model
+    mlflow.sklearn.log_model(model, "model")
+    
+    # Simpan model
+    joblib.dump(model, PREPROCESSING_DIR + 'lr_model.joblib')
+    
+    current_run_id = run.info.run_id
+
+print(f"Model Training Selesai. Run ID: {current_run_id}")
